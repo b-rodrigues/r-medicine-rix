16268b8097c587080826dd5dea819c94adc5d77b
Bruno Rodrigues <bruno@brodrigues.co>
2025-03-10 16:12:27 +0100

    first commit

---
+++ scripts/nix_expressions/rix_intro/generate_env_rstudio.R
@@ -0,0 +1,10 @@
+library(rix)
+
+rix(date = "2025-03-03",
+    r_pkgs = c("dplyr", "ggplot2"),
+    system_pkgs = NULL,
+    git_pkgs = NULL,
+    tex_pkgs = NULL,
+    ide = "rstudio",
+    project_path = ".",
+    overwrite = TRUE)
---
+++ scripts/nix_expressions/vscode_example/generate_env_vscode_win.R
@@ -0,0 +1,10 @@
+library(rix)
+
+rix(date = "2025-03-03",
+    r_pkgs = c("dplyr", "ggplot2", "languageserver"),
+    system_pkgs = NULL,
+    git_pkgs = NULL,
+    tex_pkgs = NULL,
+    ide = "none",
+    project_path = ".",
+    overwrite = TRUE)
