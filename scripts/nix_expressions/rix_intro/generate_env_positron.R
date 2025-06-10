02162ab913ae8c46f6d26a2eedd1026cc8158ce9
Bruno Rodrigues <bruno@brodrigues.co>
2025-06-10 08:44:52 +0200

    examples

--- scripts/nix_expressions/rix_intro/generate_env_positron.R
+++ scripts/nix_expressions/rix_intro/generate_env_positron.R
@@ -2,7 +2,7 @@ library(rix)
 
 rix(date = "2025-03-03",
     r_pkgs = c("dplyr", "ggplot2"),
-    system_pkgs = NULL,
+    system_pkgs = c("python312", "python312Packages.pandas"),
     git_pkgs = NULL,
     tex_pkgs = NULL,
     ide = "positron",
